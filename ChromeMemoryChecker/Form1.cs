f818048277be521409186913d3d0a254ee28c3db
Netopya <netopya@netopyaplanet.com>
2016-10-12 15:53:38 -0400

    Simple Chrome Memory Summation
    -Check for chrome processes and add up their memory

---
+++ ChromeMemoryChecker/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace ChromeMemoryChecker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var process = Process.GetProcesses();
+            var chrome = process.Where(x => x.ProcessName.Contains("chrome"));
+
+            Console.WriteLine(string.Join(System.Environment.NewLine, chrome.Select(x => x.ProcessName)));
+
+            var mems = chrome.Select(x => x.PrivateMemorySize64);
+            long memb = mems.Aggregate((long)0, (x, y) => x + y);
+            double memkb = ((double)memb) / (double)1024.0;
+            double memgb = memkb / 1024.0 / 1024.0;
+            Console.WriteLine(memgb.ToString());
+        }
+    }
+}
