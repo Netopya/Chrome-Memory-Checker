03fa5e46563763f7c6316a2b2e616e641cc4fd90
Michael B <netopya@netopyaplanet.com>
2016-10-13 12:46:34 -0400

    Added basic listview gui

--- ChromeMemoryChecker/Form1.Designer.cs
+++ ChromeMemoryChecker/Form1.Designer.cs
@@ -28,21 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lstvMain = new System.Windows.Forms.ListView();
+            this.clmName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmMemory = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // lstvMain
+            // 
+            this.lstvMain.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.clmName,
+            this.clmMemory});
+            this.lstvMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lstvMain.Location = new System.Drawing.Point(3, 23);
+            this.lstvMain.Name = "lstvMain";
+            this.lstvMain.Size = new System.Drawing.Size(278, 235);
+            this.lstvMain.TabIndex = 0;
+            this.lstvMain.UseCompatibleStateImageBehavior = false;
+            this.lstvMain.View = System.Windows.Forms.View.Details;
+            // 
+            // clmName
+            // 
+            this.clmName.Text = "Process Name";
+            this.clmName.Width = 95;
+            // 
+            // clmMemory
+            // 
+            this.clmMemory.Text = "Memory Usage";
+            this.clmMemory.Width = 98;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.lstvMain, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.lblTotal, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(284, 261);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(3, 0);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(35, 13);
+            this.lblTotal.TabIndex = 1;
+            this.lblTotal.Text = "label1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView lstvMain;
+        private System.Windows.Forms.ColumnHeader clmName;
+        private System.Windows.Forms.ColumnHeader clmMemory;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label lblTotal;
     }
 }
 
--- ChromeMemoryChecker/Form1.cs
+++ ChromeMemoryChecker/Form1.cs
@@ -23,13 +23,19 @@ namespace ChromeMemoryChecker
             var process = Process.GetProcesses();
             var chrome = process.Where(x => x.ProcessName.Contains("chrome"));
 
-            Console.WriteLine(string.Join(System.Environment.NewLine, chrome.Select(x => x.ProcessName)));
+            //Console.WriteLine(string.Join(System.Environment.NewLine, chrome.Select(x => x.ProcessName)));
 
             var mems = chrome.Select(x => x.PrivateMemorySize64);
             long memb = mems.Aggregate((long)0, (x, y) => x + y);
             double memkb = ((double)memb) / (double)1024.0;
             double memgb = memkb / 1024.0 / 1024.0;
-            Console.WriteLine(memgb.ToString());
+            //Console.WriteLine(memgb.ToString());
+            
+            lblTotal.Text = string.Format("Total usage in Gb: {0:0.00}Gb over {1} processes", memgb, chrome.Count());
+
+            
+
+            lstvMain.Items.AddRange(chrome.Select(x => new ListViewItem(new[] { x.ProcessName, string.Format("{0:0.000}", x.PrivateMemorySize64 / 1024.0 / 1024.0)})).ToArray());
         }
     }
 }
